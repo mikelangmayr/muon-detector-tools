b07466f39dc4d1430d24fff7a6b05a8555cd022a
Michael Langmayr <michael.langmayr@gmail.com>
2024-11-01 15:47:12 -0700

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # muon-detector-tools
+
+This repository contains a Python-based detector script for identifying cosmic ray muons in images taken by an astronomical optical instrument. Cosmic ray muons are particles produced by high-energy cosmic rays colliding with Earth’s atmosphere. This project includes sample data and a Jupyter notebook to help you get started with detecting them.
--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from cr_catalogue.cr_catalogue import cr_catalogue
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Run muon_fit with sample data
-    muon_fit("muon_data", 0.45, 0.69, gplot=False, hplot=False, ps=True, crplot=False, verbose=False)
+    muon_fit("sample_data", 0.45, 0.69, gplot=False, hplot=False, ps=True, crplot=False, verbose=False)
     # cr_catalogue("muon_data")
